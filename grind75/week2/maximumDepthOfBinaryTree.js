5cde26d822d5e7bc3d349599dd9bdcdc097ec605
Yasaswini <yasha.balu25@gmail.com>
2023-11-12 23:56:19 +1100

    Grind75 problems-2

---
+++ grind75/week2/maximumDepthOfBinaryTree.js
@@ -0,0 +1,35 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+// Example 2:
+// Input: root = [1,null,2]
+// Output: 2
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left && !root.right) {
+    return 1;
+  }
+  let leftLength = root.left ? 1 + maxDepth(root.left) : 1;
+  let rightLength = root.right ? 1 + maxDepth(root.right) : 1;
+  return leftLength > rightLength ? leftLength : rightLength;
+};
