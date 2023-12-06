0528d73a8eac938b6ee28f3178bd769e96efa7c8
Yasaswini <yasha.balu25@gmail.com>
2023-12-06 21:46:00 +1100

    Sorting And Searching algorithm - 1

---
+++ technicalInterviewHandbook/Array/kthSmallestElementInSortedMatrix.js
@@ -0,0 +1,47 @@
+// 378. Kth Smallest Element in a Sorted Matrix
+// Medium
+// 9.6K
+// 329
+// Companies
+// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+// You must find a solution with a memory complexity better than O(n2).
+
+// Example 1:
+// Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+// Output: 13
+// Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+
+// Example 2:
+// Input: matrix = [[-5]], k = 1
+// Output: -5
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+// var kthSmallest = function(matrix, k) {
+//     let newMatrix = matrix.flat().sort((a,b) => a-b)
+//     return newMatrix[k-1]
+// };
+
+var kthSmallest = function (matrix, k) {
+  let lo = matrix[0][0],
+    hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1; // +1 because we don't want to forget the last number
+  while (lo < hi) {
+    let mid = lo + Math.floor((hi - lo) / 2);
+    let count = 0;
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[i][j] <= mid) count++;
+        else break;
+      }
+    }
+    if (count < k) lo = mid + 1;
+    else hi = mid;
+  }
+  return lo;
+};
