67efae5793b5d414fdad62818ad6d3bc8d74a6c6
Yasaswini <yasha.balu25@gmail.com>
2023-12-10 23:59:15 +1100

    problem 1

---
+++ eloquentJavascript/ProgramStructure/foobar.js
@@ -0,0 +1,11 @@
+function fooBar(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log("FooBar");
+    } else if (i % 3 === 0) {
+      console.log("Foo");
+    } else if (i % 5 === 0) {
+      console.log("Bar");
+    }
+  }
+}
