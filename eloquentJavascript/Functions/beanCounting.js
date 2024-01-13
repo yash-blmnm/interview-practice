b377415e58cbc9146807463a7507bc504bbc045d
Yasaswini <yasha.balu25@gmail.com>
2024-01-13 23:46:24 +1100

    Removing Eloquent Javascript Exercises

--- eloquentJavascript/Functions/beanCounting.js
+++
@@ -1,9 +0,0 @@
-// Your code here.
-function countChar(str, char) {
-  return str.split("").filter((x) => x === char).length;
-}
-countBs = (str) => countChar(str, "B");
-console.log(countBs("BBC"));
-// → 2
-console.log(countChar("kakkerlak", "k"));
-// → 4
--- eloquentJavascript/Functions/minValue.js
+++
@@ -1,9 +0,0 @@
-// Your code here.
-function min(val1, val2) {
-  return val1 < val2 ? val1 : val2;
-}
-
-console.log(min(0, 10));
-// → 0
-console.log(min(0, -10));
-// → -10
